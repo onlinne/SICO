530b79eb260989e2f274de699931f803e0d83935
Juan Camilo Fernandez Rendon <juancamilo.fernandez@utp.edu.co>
2021-08-28 18:04:26 -0500

    inner ventaCloset-Cliente(error de logica)

--- models/VentaCloset.js
+++ models/VentaCloset.js
@@ -1,15 +1,28 @@
 import mongoose from 'mongoose';
-import NuevoClienteCloset from './ClienteCloset.js';
 
 const SchemaVentaCloset=new mongoose.Schema({
     numeroContrato:{ type: String, required: true, unique: true},
-    Cliente:{ type: mongoose.Schema.Types.ObjectId, ref: NuevoClienteCloset, required: true},
     contrato:{ type:String, required: true},
     valorVenta:{ type: Number,required: true},
     anio:{ type: String},
     mes:{ type: String},
     dia:{ type: String},
-    editable: {type: Boolean, default: true}
+    editable: {type: Boolean, default: true},
+    cliente:{type: mongoose.Schema.Types.ObjectId, ref:'ClienteCloset'}
 });
+
+const SchemaClienteCloset=new mongoose.Schema({
+    _id:String,
+    cedula:{type: String,required: true,maxLength:10,unique: true},
+    nombre:{type: String,required: true},
+    telefono:{type: String,required: true,maxLength:10},
+    direccion:{type:String,required: true},
+    correo:{type: String,require: false},
+    venta:[{type: mongoose.Schema.Types.ObjectId, ref:'NuevaVentaCloset'}]
+});
+
+export const ClienteCloset = mongoose.model('ClienteCloset',SchemaClienteCloset);
+
+
 const NuevaVentaCloset = mongoose.model('NuevaVentaCloset',SchemaVentaCloset);
 export default NuevaVentaCloset;
